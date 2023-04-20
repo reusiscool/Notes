6e1d5d59a98a37ef673f2476f1c62bb5d3e02350
reusiscool <robert.prigara@yandex.ru>
2023-04-20 19:53:02 +0300

    time localization

--- website/static/index.js
+++ website/static/index.js
@@ -4,6 +4,13 @@ function f(){
 let ls = document.getElementsByClassName('created');
 for (const i of ls) {
     let date = new Date(i.innerHTML);
-    i.innerHTMl = date.toLocaleString();
-//    i.innerHTML = 'none';
+    i.innerHTML = 'created on ' + date.toLocaleString();
+}
+ls = document.getElementsByClassName('notify');
+for (const i of ls) {
+    let date = new Date(i.innerHTML);
+    date = date.toLocaleString();
+    if (date != 'Invalid Date'){
+        i.innerHTML = 'notification on ' + date;
+    }
 }
